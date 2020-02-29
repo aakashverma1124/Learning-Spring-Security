8ac18e2333467351d361030f6396f177c8806242
aakashverma1124 <info.aakash11@gmail.com>
2020-02-29 10:27:54 +0530

    Spring Boot Authorization is done.

--- src/main/java/in/aboutaakash/springsecurity/config/SpringSecurityConfiguration.java
+++ src/main/java/in/aboutaakash/springsecurity/config/SpringSecurityConfiguration.java
@@ -2,6 +2,7 @@ package in.aboutaakash.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -28,5 +29,14 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder getPasswordEcoder() {
 		return NoOpPasswordEncoder.getInstance();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/admin").hasRole("ADMIN")
+			.antMatchers("/user").hasAnyRole("USER", "ADMIN")
+			.antMatchers("/").permitAll()
+			.and().formLogin();
 	}
 }
